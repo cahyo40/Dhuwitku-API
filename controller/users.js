13aea8db5d51627cc02f8a88ef9fbfcfa7bdd4d2
cahyo40 <mdcnugroho@gmail.com>
2022-12-20 12:21:41 +0700

    tambah fungsi login token

--- controller/users.js
+++ controller/users.js
@@ -1,17 +1,23 @@
 var db = require('../config/database')
 var bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken')
+const { getEmail } = require('./keuangan')
 require('dotenv').config()
+
 
 exports.login = async function (req, res) {
     if (req.method !== 'POST') return res.status(405).end();
 
     const { email, password } = req.body;
     const login = await db('users').where({ email }).first();
-    if (!login) return res.status(401).end();
+    if (!login) return res.status(401)
+        .json({ message: "Login Unsuccessfully", token: null })
+        .end();
 
     const checkPassword = await bcrypt.compare(password, login.password);
-    if (!checkPassword) return res.status(401).end();
+    if (!checkPassword) return res.status(401)
+        .json({ message: "Login Unsuccessfully", token: null })
+        .end();
 
     const token = jwt.sign({
         email: login.email,
@@ -25,6 +31,18 @@ exports.login = async function (req, res) {
 
 }
 
+exports.loginToken = async function (req, res) {
+    if (req.method !== 'GET') return res.status(405).end();
+    if (!req.headers.authorization) return res.status(401).end();
+    if (req.headers.authorization.split(' ')[0] !== 'Bearer') return res.status(401).end();
+    const token = req.headers.authorization.split(' ')[1];
+    const user = jwt.verify(token, process.env.JWT_KEY);
+    const email = user['email'];
+
+    const login = await db('users').where('email', email).first();
+    res.status(200).json(login);
+}
+
 exports.register = async function (req, res) {
     if (req.method !== 'POST') return res.status(405).end();
 
@@ -36,7 +54,7 @@ exports.register = async function (req, res) {
     await db('users').insert({ email, name, username, password: passwordHash });
     const data = await db('users').where({ email }).first();
 
-    res.status(200).json({ message: "Created user successfully", data: data })
+    res.status(200).json(data)
 
 
 }
